317cda286df98cfea75590902b198dd936f55f0d
Andrei Malashkin <andrei.malashkin@nil.foundation>
2024-10-11 16:18:30 +0200

    correct exacmple

--- .github/workflows/nightly.nix
+++ .github/workflows/nightly.nix
@@ -1,12 +1,15 @@
-name: Nightly Testing
-
+name: example-cron
 on:
   schedule:
+    # runs tests every day at 4am
     - cron: "*/5 * * * *"
+  workflow_dispatch:
 
 jobs:
-  jobname:
-    name: Echo
-    run |
-      echo "job was ran!"
+  nightly:
+    runs-on: ubuntu-24.04
+    steps:
+      - name: Checkout
+      run |
+        echo "job was ran!"
 
